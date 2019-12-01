1601f76400939d4a84282f9475eb6fcb59a26037
Macbie <kongzhengho@Kongs-MacBook-Air.local>
2019-12-01 20:32:28 +0800

    Initial release

---
+++ client/src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar = () => {
+    return ( 
+        <nav className="navbar navbar-dark bg-dark mb-2 shadow-sm">
+            <span className="navbar-brand">Object Detection</span>
+        </nav>
+    );
+}
+ 
+export default NavBar;
---
+++ server/server.js
@@ -0,0 +1,67 @@
+const http=require('http')
+const express = require('express')
+const app = express()
+const socketio = require('socket.io')
+const PORT = process.env.PORT || 5000;
+const VisualRecognitionV3 = require('ibm-watson/visual-recognition/v3');
+const { IamAuthenticator } = require('ibm-watson/auth');
+var cron = require('node-cron');
+
+const visualRecognition = new VisualRecognitionV3({
+    version: '2018-03-19',
+    authenticator: new IamAuthenticator({
+    apikey: 'Se157Ca9M4TbX3vltfwtogsPnDk0JCfp-9y2ZpR9swsg',
+    }),
+    url: 'https://gateway.watsonplatform.net/visual-recognition/api',
+});
+        
+const server = http.createServer(app).listen(PORT, function() {
+    console.log(`Listening at: http://localhost:${PORT}`);
+});
+const io = socketio(server);
+
+// Initiate a socket connection
+io.on('connection', function(socket){
+    console.log('new connection');
+
+    // Listen upload image request from client side
+    socket.on('upload image', function(img){
+        
+        const classifyParams = {
+            imagesFile: img,
+        };
+
+        visualRecognition.classify(classifyParams)
+            .then(response => {
+                const classifiedImages = response.result;
+                console.log(JSON.stringify(classifiedImages, null, 2));
+                const detectedClasses = classifiedImages.images[0].classifiers[0].classes.map(classObj => classObj.class);
+                socket.emit('image classes', detectedClasses);
+            })
+            .catch(err => {
+                console.log('error:', err);
+                socket.emit('error', err);
+            });
+    });
+});
+
+// Schedule a monthly request to IBM Watson Visual Recognition API
+cron.schedule('00 00 12 29 * *', () => {
+    console.log('Runing a job at 12:00 P.M. 29th of every month');
+    
+    const classifyParams = {
+        url: 'https://ibm.biz/BdzLPG',
+    };
+      
+    visualRecognition.classify(classifyParams)
+    .then(response => {
+        const classifiedImages = response.result;
+        console.log(JSON.stringify(classifiedImages, null, 2));
+    })
+    .catch(err => {
+        console.log('error:', err);
+    });        
+  }, {
+    scheduled: true,
+    timezone: 'Asia/Kuala_Lumpur'
+});
